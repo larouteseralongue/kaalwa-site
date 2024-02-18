bb200725f092e5c6b4dc4d6b8a6033e41a4fe211
larouteseralongue <95699128+larouteseralongue@users.noreply.github.com>
2024-02-17 18:19:08 -0800

    Add files via upload
    Premiere version

---
+++ kaalwa/devoir/include/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer py-4">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 text-lg-left" id="annee"></div>
+
+                        <script>
+                            var anneeActuelle = new Date().getFullYear();
+                            document.getElementById("annee").innerText = "© " + anneeActuelle;
+                        </script>
+                        <div class="col-lg-4 my-3 my-lg-0">
+                            <p>
+                                Abonnez-Vous Sur:
+                            </p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/channel/UCFjGn84DvD5Q4XoeZbwwKdw"><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.deezer.com/fr/artist/103778172"><i class="fab fa-deezer"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://artists.spotify.com/c/artist/1k2ZWpeCEl1xjJBq67iNdd/profile/overview"><i class="fab fa-spotify"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/kaalwa_authentique"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/KaalwaAuthentique"><i class="fab fa-facebook-f"></i></a>
+                        </div>
+                       
+                    </div>
+                </div>
+</footer>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<!-- Contact form JS-->
+<script src="devoir/mail/jqBootstrapValidation.js"></script>
+<script src="devoir/mail/contact_me.js"></script>
+<!-- Core theme JS-->
+<script src="devoir/js/scripts.js"></script>
---
+++ kaalwa/devoir/include/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--[if lt IE 9]>
+<script
+src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>KaalwaAuthentique</title>
+     <link rel="icon" type="image/x-icon" href="devoir/assets/img/favicon.ico" />
+     
+     <script src="https://kit.fontawesome.com/14273d579a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="bg-dark">
+        <div class="contenaire" id="menu">
+            <div class="row">
+                <nav class="col navbar navbar-expand-lg navbar-dark">
+                    <a class="navbar-brand" href="index.html"> <img class="card-img-top" src="devoir/assets/img/sof.png" alt="Kaalwa" width="45" height="30"></a>
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarContent">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div id="navbarContent" class="collapse navbar-collapse">
+                        <ul class="navbar-nav albus">
+                            <li class="nav-item active cercle">
+                            <a class="nav-link js-scroll-trigger" href="index.php">Accueil</a>
+                            </li>
+                            <li class="nav-item active cercle">
+                                <a class="nav-link js-scroll-trigger" href="discovery.php">Gallerie</a>
+                            </li>
+                           
+                            <li class="nav-item active cercle">
+                                <a class="nav-link js-scroll-trigger" href="discovery.php#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
